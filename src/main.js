845f6596c24d2f0232768eac2e3902ac5c866950
Ivan Zhuravlev <ru31337@gmail.com>
2018-06-27 19:16:26 +0300

    rpc over ws

--- src/main.js
+++ src/main.js
@@ -14,10 +14,48 @@ import './assets/css/global.css'
 
 Vue.use(BootstrapVue);
 
-Vue.use(VueNativeSock,
-  //'ws://88.99.87.200:1555',
-  'ws://demos.kaazing.com/echo',
-  { store: StoreWS, format: 'json'})
+                    // 'ws://88.99.87.200:1555'
+Vue.use(VueNativeSock, 'ws://demos.kaazing.com/echo', {
+  store: StoreWS,
+  format: 'json',
+  ids: 1,
+  sendRpc (method, params) {
+    return this.WebSocket.sendObj({
+      jsonrpc: "2.0",
+      method: method,
+      params: params,
+      id: ++this.opts.ids
+    })
+  },
+  passToStoreHandler (eventName, event) {
+    console.log('[WS]:', eventName, event)
+    if(eventName === 'SOCKET_onopen') {
+      setInterval(() => {
+        this.opts.sendRpc.call(this, "enq_getAllTransactions", {
+          address:"5YXZXhM9MMafhS5fdzanWbimKEpAnmrqabD5BcMm8TxS"
+        })
+      }, 5000)
+    }
+    // rpc
+    //this.emit(eventName, event)
+
+    //if (!eventName.startsWith('SOCKET_')) { return }
+    /*let method = 'commit'
+    let target = eventName.toUpperCase()
+    let msg = event
+    if (this.format === 'json' && event.data) {
+      msg = JSON.parse(event.data)
+      if (msg.mutation) {
+        target = [msg.namespace || '', msg.mutation].filter((e) => !!e).join('/')
+      } else if (msg.action) {
+        method = 'dispatch'
+        target = [msg.namespace || '', msg.action].filter((e) => !!e).join('/')
+      }
+    }
+    this.store[method](target, msg)
+    */
+  }
+})
 
 Vue.config.productionTip = false
 
