4c92a57df446ce11146f0684ef6b0b67dd8b2d96
Ivan Zhuravlev <ru31337@gmail.com>
2018-07-01 00:48:20 +0300

    CI

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,8 +3,5 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  devServer: {
-    disableHostCheck: true
-  }
+  NODE_ENV: '"development"'
 })
